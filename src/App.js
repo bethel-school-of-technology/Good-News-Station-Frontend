eb4ab4be09fa15ff5753e56f8e63bd36b38afe5f
Nelson Chaves <nelsonchavespro@gmail.com>
2020-04-28 18:29:04 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => (
+  <Fragment>
+    <h1>Welcome to the Good News Station!</h1>
+  </Fragment>
+)
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const navbar = props => (
+    <header>
+        <nav>
+            <div>Home Page</div>
+            <div>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">My Profile</a></li>
+                    <li><a href="/">Newsfeed</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+export default navbar;
