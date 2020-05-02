0c7485e4a6adb45465f24616072793a423d4a7dd
Charles Bartels <charlieb2898@gmail.com>
2020-05-02 10:26:23 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
-import Navbar from './components/Navbar/Navbar';
-
 const App = () => (
   <Fragment>
     <h1>Welcome to the Good News Station!</h1>
