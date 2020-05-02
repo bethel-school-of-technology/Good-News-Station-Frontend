4e79588b6b02305c1111816bc61c6c3c7b587ea0
Jeb <lassojeb@gmail.com>
2020-05-02 15:11:08 -0600

    work on posts page and Landing integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Posts from './components/posts/Posts';
 
 const App = () => (
+  <Router>
   <Fragment>
     <Navbar />
-    <Landing />
+    <Route exact path='/' component={Landing} />
+    <Route exact path='/posts' component={Posts} />
+{/* 
+    <section className="container">
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
+    </section>
+ */}
     <h1>Temp Header</h1>
   </Fragment>
+  </Router>
 )
 
 export default App;
