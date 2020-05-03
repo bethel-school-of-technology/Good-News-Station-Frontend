6ccfbc6465cd5d55f758032f793b52beb44088e6
Nelson Chaves <nelsonchavespro@gmail.com>
2020-05-02 18:27:17 -0700

    store created, CSS added, Alert created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
 
-const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-    <h1>Temp Header</h1>
-  </Fragment>
-)
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
