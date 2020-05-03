426ed8cb83744349a962b1525dfd43720289c72c
JustinH <jharris@escgov.com>
2020-05-03 00:34:09 -0400

    Changed wording under email box

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -56,8 +56,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
             onChange={e => onChange(e)}
           />
           <small className='form-text'>
-            This site uses Gravatar so if you want a profile image, use a
-            Gravatar email
+            Good News is all around you =)
           </small>
         </div>
         <div className='form-group'>
