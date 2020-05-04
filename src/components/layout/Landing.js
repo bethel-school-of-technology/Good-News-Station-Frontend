55c2fbf84cc96fb22037bca4721c020f1d6e42e5
Jeb <lassojeb@gmail.com>
2020-05-04 00:47:09 -0600

    fixed posts errors add posts pages

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Posts } from "../posts/Posts";
+import PostItem from "../posts/PostItem";
+import { getPosts } from "../../actions/post";
 
-const Landing = () => {
+const Landing = ({ getPosts, posts }) => {
   return (
     <section className="container">
-      <div className="overlay">
-        <div className="landing-inner">
-          <h1 className="title">Welcome to Good News Station!</h1>
-          <Posts />
-          <h4 className="subHeader">
+      <div className="">
+        <div className="">
+          <h1 className="large text-primary">Welcome to Good News Station!</h1>
+          <h4 className="">
             Please create an account or sign in to create a post and start
             adding some good news.
           </h4>
+
+          {/* LIST OF POSTS */}
+
+          <Fragment>
+            <h1 className="">Some Current Good News</h1>
+            <p className="lead"></p>
+
+            {/* POSTS LISTS */}
+
+            <div className="posts">
+              {(post) => <PostItem key={post._id} post={post} />}
+            </div>
+          </Fragment>
+
+          {/* HOME PAGE LINKS */}
+
           <div className="mainButtons">
             <Link to="/register" className="btn-Primary">
               Create an Account
             </Link>
+            {" | "}
             <Link to="/login" className="btn-Primary">
               Sign In
             </Link>
