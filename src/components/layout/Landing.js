903bdb0e2065a661e70eb6c1bbe22cb7b88b835b
Nelson Chaves <nelsonchavespro@gmail.com>
2020-05-04 01:14:59 -0700

    Clean Up

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -8,7 +8,7 @@ const Landing = ({ getPosts, posts }) => {
     <section className="container">
       <div className="">
         <div className="">
-          <h1 className="large text-primary">Welcome to Good News Station!</h1>
+          <h1 className="large text-primary">Welcome to the Good News Station!</h1>
           <h4 className="">
             Please create an account or sign in to create a post and start
             adding some good news.
