1dfb709e9c713c6e0d0017cc8d7832bbcb00d58c
Charles Bartels <charlieb2898@gmail.com>
2020-05-04 18:46:54 -0500

    created basic ContactUs.js. added logout action to Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-/*still needs logout action created and imported here */
+import { logout } from '../../actions/auth';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
@@ -68,5 +68,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  
+    { logout }
 )(Navbar);
