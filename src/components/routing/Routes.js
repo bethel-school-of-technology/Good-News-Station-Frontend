ae7134624f3b4f6287d50c8b4ab050d295b208f9
Nelson Chaves <nelsonchavespro@gmail.com>
2020-05-04 13:40:15 -0700

    Not Found page created

--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -7,10 +7,10 @@ import Alert from '../layout/Alert';
 // import ProfileForm from '../profile-forms/ProfileForm';
 // import Profiles from '../profiles/Profiles';
 // import Profile from '../profile/Profile';
-import Posts from '../posts/Posts';
-import Post from '../post/Post';
-// import NotFound from '../layout/NotFound';
-import PrivateRoute from '../routing/PrivateRoute';
+// import Posts from '../posts/Posts';
+// import Post from '../post/Post';
+import NotFound from '../layout/NotFound';
+// import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = props => {
   return (
@@ -20,14 +20,13 @@ const Routes = props => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         {/* <Route exact path="/profiles" component={Profiles} />
-        <Route exact path="/profile/:id" component={Profile} /> */}
-        {/* <PrivateRoute exact path="/dashboard" component={Dashboard} /> */}
-        {/* <PrivateRoute exact path="/create-profile" component={ProfileForm} />
+        <Route exact path="/profile/:id" component={Profile} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
-
         <Route exact path="/posts" component={Posts} />
-        <Route exact path="/posts/:id" component={Post} /> 
-        {/* <Route component={NotFound} /> */}
+        <Route exact path="/posts/:id" component={Post} />  */}
+        <Route component={NotFound} />
       </Switch>
     </section>
   );
