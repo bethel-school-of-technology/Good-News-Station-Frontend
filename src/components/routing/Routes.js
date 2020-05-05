882b2b6be6f235adcb33c9592af1252ab9dd7b51
Nelson Chaves <nelsonchavespro@gmail.com>
2020-05-05 16:43:55 -0700

    Create & Edit profile form

--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -4,7 +4,7 @@ import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Alert from '../layout/Alert';
 import Dashboard from '../dashboard/Dashboard';
-// import ProfileForm from '../profile-forms/ProfileForm';
+import ProfileForm from '../profile-forms/ProfileForm';
 // import Profiles from '../profiles/Profiles';
 // import Profile from '../profile/Profile';
 // import Posts from '../posts/Posts';
@@ -22,10 +22,11 @@ const Routes = props => {
         {/* <Route exact path="/profiles" component={Profiles} />
         <Route exact path="/profile/:id" component={Profile} /> */}
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        {/* <PrivateRoute exact path="/create-profile" component={ProfileForm} />
+        <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
-        <PrivateRoute exact path="/posts" component={Posts} />
+        {/* <PrivateRoute exact path="/posts" component={Posts} />
         <PrivateRoute exact path="/posts/:id" component={Post} /> */}
+
         <Route component={NotFound} />
       </Switch>
     </section>
