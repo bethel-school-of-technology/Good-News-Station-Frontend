42135260f753f2a9fabdc22ae71f806e017b98f1
Nelson Chaves <nelsonchavespro@gmail.com>
2020-05-06 00:18:29 -0700

    List of Users done

--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -5,7 +5,7 @@ import Login from '../auth/Login';
 import Alert from '../layout/Alert';
 import Dashboard from '../dashboard/Dashboard';
 import ProfileForm from '../profile-forms/ProfileForm';
-// import Profiles from '../profiles/Profiles';
+import Profiles from '../profiles/Profiles';
 // import Profile from '../profile/Profile';
 // import Posts from '../posts/Posts';
 // import Post from '../post/Post';
@@ -19,8 +19,8 @@ const Routes = props => {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        {/* <Route exact path="/profiles" component={Profiles} />
-        <Route exact path="/profile/:id" component={Profile} /> */}
+        <Route exact path="/profiles" component={Profiles} />
+        {/* <Route exact path="/profile/:id" component={Profile} /> */}
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
