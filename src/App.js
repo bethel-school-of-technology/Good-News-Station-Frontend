90a8cc82af4088e5537ca94665a3563df7a8e90d
Joshua Gray <gohan3302@gmail.com>
2020-05-07 17:55:54 -0500

    profile.js displaying

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
-import Profile from './components/profile/Profile';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -27,7 +26,6 @@ const App = () => {
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route component={Routes} />
-            <Route exact path='/profile/:id' component={Profile} />
           </Switch>
         </Fragment>
       </Router>
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,9 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
@@ -12,31 +10,10 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
     }, [getProfileById, match.params.id]);
 
     return (
-        <Fragment>
-            {profile === null ? (
-                <Spinner />
-            ) : (
-                    <Fragment>
-                        <Link to="/profiles" className="btn btn-light">
-                            Back To Profiles
-          </Link>
-                        {auth.isAuthenticated &&
-                            auth.loading === false &&
-                            auth.user._id === profile.user._id && (
-                                <Link to="/edit-profile" className="btn btn-dark">
-                                    Edit Profile
-                                </Link>
-                            )}
-
-
-
-
-
-
-                    </Fragment>
-                )}
-        </Fragment>
-    );
+        <div>
+            profile
+        </div>
+    )
 };
 
 Profile.propTypes = {
@@ -45,9 +22,9 @@ Profile.propTypes = {
     auth: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
     profile: state.profile,
     auth: state.auth
 });
 
-export default connect(mapStateToProps, { getProfileById })(Profile);
+export default connect(mapStateToProps, { getProfileById })(Profile)
