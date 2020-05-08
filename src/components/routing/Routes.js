b67b05221e27d04173d5c24052018a0e85ce0b3a
Nelson Chaves <nelsonchavespro@gmail.com>
2020-05-07 22:49:09 -0700

    fixed Post.js

--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -7,8 +7,8 @@ import Dashboard from '../dashboard/Dashboard';
 import ProfileForm from '../profile-forms/ProfileForm';
 import Profiles from '../profiles/Profiles';
 // import Profile from '../profile/Profile';
-// import Posts from '../posts/Posts';
-// import Post from '../post/Post';
+import Posts from '../posts/Posts';
+import Post from '../post/Post';
 import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
 
@@ -24,8 +24,8 @@ const Routes = props => {
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
-        {/* <PrivateRoute exact path="/posts" component={Posts} />
-        <PrivateRoute exact path="/posts/:id" component={Post} /> */}
+        <PrivateRoute exact path="/posts" component={Posts} />
+        <PrivateRoute exact path="/posts/:id" component={Post} />
 
         <Route component={NotFound} />
       </Switch>
