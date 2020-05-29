cc255c7e3b2f505e6806c932f0958df64d496a75
jharristech <jharris@escgov.com>
2020-05-28 21:50:06 -0400

    finalized flipback and linked to profile in db

--- src/components/contact/About.js
+++ src/components/contact/About.js
@@ -31,10 +31,9 @@ const About = () => {
                     </div>
                       <div class="flip-card-back">
                 <h3>Architect & Engineer</h3>
-                <p className="aboutFlipBack">We love that guy</p>
-                <br />
+                <strong><p className="aboutFlipBack">We love that guy</p></strong>
                 <p>I would rather be hated for who I am, than loved for who I am not.</p>
-                <img src="https://lh3.googleusercontent.com/proxy/04S0PPcCy-JAz3tyzhNAP8aUUv6g4KS7VgI-r07R7FObhG72PgpsLgulPV7g7dD0e3LvpX_Xpq4pXAxkTehjTfwh9bvTKkzA1NR1Cw" alt="Joshua-Grey" />
+                <a href="https://github.com/Joshiba26" target="_blank"><img src="https://lh3.googleusercontent.com/proxy/04S0PPcCy-JAz3tyzhNAP8aUUv6g4KS7VgI-r07R7FObhG72PgpsLgulPV7g7dD0e3LvpX_Xpq4pXAxkTehjTfwh9bvTKkzA1NR1Cw" alt="Joshua-Grey" /></a>
               </div>
             </div>
           </div>
@@ -54,11 +53,10 @@ const About = () => {
                 <img class="imgAB" src="https://avatars1.githubusercontent.com/u/55574734?s=460&v=4" alt="Nelson-Chaves" />
               </div>
               <div class="flip-card-back">
-              <h3>Architect & Engineer</h3>
-                <h3><a href="https://github.com/nelsonchaves" target="_blank" className="aboutFlipBack">Github</a></h3>
-                <p>I'm Cool</p>
-                <br />
-                <p>I would rather be hated for who I am, than loved for who I am not.</p>
+              <h3>Engineer & Backend</h3>
+                <strong><p className="aboutFlipBack">I'm Cool</p></strong>
+                <p>Work hard play hard.</p>
+                <a href="https://github.com/nelsonchaves" target="_blank"><img src="https://lh3.googleusercontent.com/proxy/04S0PPcCy-JAz3tyzhNAP8aUUv6g4KS7VgI-r07R7FObhG72PgpsLgulPV7g7dD0e3LvpX_Xpq4pXAxkTehjTfwh9bvTKkzA1NR1Cw" alt="Nelson-Chaves" /></a>
               </div>
             </div>
           </div>
@@ -67,17 +65,18 @@ const About = () => {
 <div className="container centerMe">
           <div class="flip-card ">
             <h2>
-            <Link to="https://github.com/jharristech" target="_blank">Justin Harris</Link>
+            <Link to="profile/5ed04968e8546f5e2cbf3dfb" target="_blank">Justin Harris</Link>
           </h2>
             <div class="flip-card-inner">
               <div class="flip-card-front">
                 <img class="imgAB" src="https://avatars2.githubusercontent.com/u/55962297?s=460&u=cc21587c51614db5c9db6860190b0a2e2a96e246&v=4" alt="Justin" />
               </div>
               <div class="flip-card-back">
-                <h1>Geb Brant</h1>
-                <Link to="https://github.com/Lassojeb" target="_blank">Jeb Brant<br /></Link>
-                <p>Architect & Engineer</p>
-                <p>We love that guy</p>
+                <h3>Fullstack</h3>
+                <strong><p className="aboutFlipBack">Be Greatfull</p></strong>
+                <p>Be gratefull of what you have and don't worry about what you don't</p>
+                <a href="https://github.com/jharristech" target="_blank"><img src="https://lh3.googleusercontent.com/proxy/04S0PPcCy-JAz3tyzhNAP8aUUv6g4KS7VgI-r07R7FObhG72PgpsLgulPV7g7dD0e3LvpX_Xpq4pXAxkTehjTfwh9bvTKkzA1NR1Cw" alt="Justin" /></a>
+              
               </div>
             </div>
           </div>
@@ -86,17 +85,18 @@ const About = () => {
           <div className="container centerMe">
           <div class="flip-card ">
             <h2>
-            <Link to="https://github.com/Lassojeb" target="_blank">Jeb Brant</Link>
+            <Link to="profile/5ecedc2815afeb58d0da5873" target="_blank">Jeb Brant</Link>
           </h2>
             <div class="flip-card-inner">
               <div class="flip-card-front">
-                <img class="imgAB" src="https://avatars3.githubusercontent.com/u/55933461?s=460&u=0bea4a90c8bc05e19c5048aebfc51e4a6d659cd2&v=4" alt="Joshua-Grey" />
+                <img class="imgAB" src="https://avatars3.githubusercontent.com/u/55933461?s=460&u=0bea4a90c8bc05e19c5048aebfc51e4a6d659cd2&v=4" alt="Jeb" />
               </div>
               <div class="flip-card-back">
-                <h1>Geb Brant</h1>
-                <Link to="https://github.com/Lassojeb" target="_blank">Jeb Brant<br /></Link>
-                <p>Architect & Engineer</p>
-                <p>We love that guy</p>
+                <h1>Architect & Engineer</h1>
+                <strong><p className="aboutFlipBack">I have lots of Good News</p></strong>
+                <p>I can do this all day.</p>
+                <a href="https://github.com/Lassojeb" target="_blank"><img src="https://lh3.googleusercontent.com/proxy/04S0PPcCy-JAz3tyzhNAP8aUUv6g4KS7VgI-r07R7FObhG72PgpsLgulPV7g7dD0e3LvpX_Xpq4pXAxkTehjTfwh9bvTKkzA1NR1Cw" alt="Jeb" /></a>
+                
               </div>
             </div>
            </div>
@@ -105,17 +105,17 @@ const About = () => {
           <div className="container centerMe">
           <div class="flip-card ">
             <h2>
-            <Link to="https://github.com/CharlesB8" target="_blank">CharlesB8</Link>
+            <Link to="profile/5ecedfb615afeb58d0da5874" target="_blank">CharlesB8</Link>
           </h2>
             <div class="flip-card-inner">
               <div class="flip-card-front">
                 <img class="imgAB" src="https://avatars3.githubusercontent.com/u/54374358?s=460&v=4" alt="Charles" />
               </div>
               <div class="flip-card-back">
-                <h1>Geb Brant</h1>
-                <Link to="https://github.com/Lassojeb" target="_blank">Jeb Brant<br /></Link>
-                <p>Architect & Engineer</p>
-                <p>We love that guy</p>
+                <h3>Engineer & Backend</h3>
+                <strong><p className="aboutFlipBack">Great things will happen</p></strong>
+                <p>This is the way</p>
+                <a href="https://github.com/CharlesB8" target="_blank"><img src="https://lh3.googleusercontent.com/proxy/04S0PPcCy-JAz3tyzhNAP8aUUv6g4KS7VgI-r07R7FObhG72PgpsLgulPV7g7dD0e3LvpX_Xpq4pXAxkTehjTfwh9bvTKkzA1NR1Cw" alt="Charles" /></a>
               </div>
             </div>
            </div>
